0eed27d6aa59edc06241bca3443b33ab8926e055
Cian O'Gralaigh <ihatecian@gmail.com>
2019-04-24 20:17:07 +0100

    add server, add routes, views, static serving

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const main = require('./routes/main');
+const users = require('./routes/users');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/users', users);
+
+app.use('/', main);
+
+app.listen(3000);
